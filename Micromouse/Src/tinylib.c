ae04a96dc7bb5bfd18ab9e8f503a6b2b4f9f5e08
Karol Trzciński <aklip0@o2.pl>
2016-04-27 17:44:57 +0200

    tinylib

---
+++ Micromouse/Src/tinylib.c
@@ -0,0 +1,26 @@
+#include "tinylib.h"
+
+/*uint8_t abs(int x)
+{
+	if(x >= 0)
+		return x;
+	else
+		return -x;
+}*/
+
+int8_t sgn(int x)
+{
+	return (x>0) - (x<0);
+}
+
+
+void _delay_us(int t)
+{
+	
+}
+
+
+void _delay_ms(int t)
+{
+	
+}
